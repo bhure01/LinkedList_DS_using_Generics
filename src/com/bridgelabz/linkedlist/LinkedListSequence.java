2421eac54b2eff8e4feb11f60404a0cfeba8c5dc
bhure01 <dattatribhure@gmail.com>
2022-01-18 17:35:15 +0530

    Modified: added first Node 70,then 30, and then 56 in Linked List

--- src/com/bridgelabz/linkedlist/LinkedListSequence.java
+++ src/com/bridgelabz/linkedlist/LinkedListSequence.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedListDemo {
+public class LinkedListSequence {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList Data Structure Problem using Java Generics");
 
@@ -9,5 +9,8 @@ public class LinkedListDemo {
         linkedList.insertFirst(70);  // insert 70 in LinkedList
         linkedList.insertFirst(30);  // insert 50 in LinkedList
         linkedList.insertFirst(56);  // insert 36 in LinkedList
+
+
+        linkedList.showLinkedList();          //showing added data in Linked List
     }
 }
