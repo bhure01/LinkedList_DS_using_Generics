a9e99d9819b88616e2697133412d6eb4ce0f70a7
bhure01 <dattatribhure@gmail.com>
2022-01-18 17:50:46 +0530

    Modified: Added 70,then 30, then 56 from last Node in LinkedList sequence

--- src/com/bridgelabz/linkedlist/LinkedListSequence.java
+++ src/com/bridgelabz/linkedlist/LinkedListSequence.java
@@ -6,11 +6,14 @@ public class LinkedListSequence {
 
         LinkedList linkedList = new LinkedList();
 
-        linkedList.insertFirst(70);  // insert 70 in LinkedList
-        linkedList.insertFirst(30);  // insert 50 in LinkedList
-        linkedList.insertFirst(56);  // insert 36 in LinkedList
+//        linkedList.insertFirst(70);  // insert 70 in LinkedList
+//        linkedList.insertFirst(30);  // insert 50 in LinkedList
+//        linkedList.insertFirst(56);  // insert 36 in LinkedList
 
+        linkedList.insertLast(70);  // insert 70 in LinkedList from last
+        linkedList.insertLast(30);  // insert 50 after 70 in LinkedList from last
+        linkedList.insertLast(56);  // insert 36 after 50 in LinkedList from last
 
-        linkedList.showLinkedList();          //showing added data in Linked List
+        linkedList.showLinkedList();       //showing added data in Linked List
     }
 }
